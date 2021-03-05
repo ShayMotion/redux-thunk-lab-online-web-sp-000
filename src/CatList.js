8585ed9b4c250c6f23b8ca13c971f41dbb84d89a
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 17:15:47 +0000

    Automatically backed up by Learn

--- src/CatList.js
+++ src/CatList.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const CatList = (props) => {
  const catpics = props.catPics.map( (cat) => {
-   return <img src={cat.url} key={cat.id} />
- })
+   return (
+     <img src={cat.url} key={cat.id} />
+  )
+ }
  
   
   return (
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,13 +1,14 @@
-import fetch from 'isomorphic-fetch'
-export function fetchCats(){
-  return function(dispatch){
-    dispatch({type: 'LOADING_CATS'})
-    return fetch('http://localhost:3000/db')
-      .then(response =>{
-        return response.json()
+import fetch from 'isomorphic-fetch';
+
+export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS'});
+    return fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+      .then(response => {
+        return response.json();
       })
-      .then(responseJson => {
-        dispatch({type: 'FETCH_CATS', payload: responseJson.images})
-      })
-  }
-}
+      .then(responseJSON => {
+        dispatch({ type: 'ADD_CATS', cats: responseJSON.images });
+      });
+  };
+};
