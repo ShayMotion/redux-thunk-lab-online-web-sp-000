484631bfd484993ae613446131aac2a1c823f1e3
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 17:16:09 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,14 +1,11 @@
-import fetch from 'isomorphic-fetch';
-
 export const fetchCats = () => {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_CATS'});
-    return fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
-      .then(response => {
-        return response.json();
-      })
-      .then(responseJSON => {
-        dispatch({ type: 'ADD_CATS', cats: responseJSON.images });
-      });
-  };
-};
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_CATS'})
+
+        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+        .then(response => response.json())
+        .then(responseJSON => {
+            dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+        })
+    }
+}
